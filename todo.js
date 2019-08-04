f6390471bcfcc0831faeec2641834bc8343d9f92
coffeemori92 <coffeemori92@naver.com>
2019-08-04 19:39:18 +0900

    Add delete btn

--- todo.js
+++ todo.js
@@ -2,7 +2,19 @@ const toDoForm  = document.querySelector(".js-toDoForm"),
       toDoInput = toDoForm.querySelector("input"),
       toDoList  = document.querySelector(".js-toDoList");
 const TODOS_LS = "toDos";
-const toDos = [];
+
+let toDos = [];
+
+function deleteToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
 
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -12,6 +24,7 @@ function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     span.innerText = text;
